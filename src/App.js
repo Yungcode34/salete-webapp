33afe149568d55f453d8c2a5bca05003146368a6
butterrzz <38452206+butterrzz@users.noreply.github.com>
2020-01-12 14:38:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
 
+import HomePage from "./Pages/homepage.component"
 function App() {
   return (
     <div>
-      Hello World
+     <HomePage/>
     </div>
   );
 }
--- src/Pages/homepage.component.jsx
+++ src/Pages/homepage.component.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import './homepage.styles.scss'
 const HomePage = () => {
   return (
     <div className="homepage">
       <div className="directory-menu">
-          <div className="menu-item">
+        <div className="menu-item">
           <div className="content">
-                <h1 className="title">Bags</h1>
-                <h1 className="subtitle">Cop now</h1>
+            <h1 className="title">Bags</h1>
+            <span className="subtitle">Cop now</span>
           </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">T-Shirts</h1>
+            <span className="subtitle">Cop now</span>
           </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Art</h1>
+            <span className="subtitle">Cop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SLT</h1>
+            <span className="subtitle">Cop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">PDC</h1>
+            <span className="subtitle">Cop now</span>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default HomePage;
