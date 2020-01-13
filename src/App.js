ea9310d222babd2594e24405dbc84da5a0d4f932
Andres <andrescardenas34@outlook.com>
2020-01-13 00:49:31 -0500

    navigation complete just have to continue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from "./Pages/homepage/homepage.component"
+const TeePage = () =>(
+  <div>
+    <h1>Tee Page</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/t-shirts" component={TeePage} />
+      </Switch>
     </div>
   );
 }
