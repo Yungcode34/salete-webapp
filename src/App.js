d90efa2c21702ef599e4cf47cf3e7b21a8323d54
Andres <andrescardenas34@outlook.com>
2020-01-12 23:48:26 -0500

    homepage broken down and styled the menu component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './App.css'
 import HomePage from "./Pages/homepage/homepage.component"
 function App() {
   return (
--- src/Pages/homepage/homepage.component.jsx
+++ src/Pages/homepage/homepage.component.jsx
@@ -1,40 +1,10 @@
 import React from "react";
+import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Bags</h1>
-            <span className="subtitle">Cop now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">T-Shirts</h1>
-            <span className="subtitle">Cop now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Art</h1>
-            <span className="subtitle">Cop now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SLT</h1>
-            <span className="subtitle">Cop now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">PDC</h1>
-            <span className="subtitle">Cop now</span>
-          </div>
-        </div>
-      </div>
+        <Directory />
     </div>
   );
 };
