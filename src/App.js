bf96cff97638b4c60ef7a3baa5b63861aa777b1e
Andres Cardenas <andrescardenas34@outlook.com>
2020-01-13 17:54:20 -0500

    pages coming out slowly but surely

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from "./Pages/homepage/homepage.component"
-const TeePage = () =>(
-  <div>
-    <h1>Tee Page</h1>
-  </div>
-)
+import ShopPage from './Pages/shop/shop.component'
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/t-shirts" component={TeePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
